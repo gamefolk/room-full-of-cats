7ccad5cfc5508f46dbcbb61eab5d3ac36cf27f5f
Andy Russell <arussell123@gmail.com>
2013-04-13 05:40:04 -0700

    fixed backaground

--- hack.c
+++ hack.c
@@ -19,7 +19,7 @@ void main()
 	LCDC_REG = 0x01; //  [FF40] = 0b00000001 = display off, window tile map = 9800-9BFF, window off, BG tile data @ 8800-97FF, BG tile map @ 9800-9BFF, sprite size = 8x8, sprites off, BG on
 	BGP_REG = OBP0_REG = OBP1_REG = 0xE4U; // [FF47] = [FF48] = [FF49] = 11100100 = 3 2 1 0 = Black, Dark Grey, Light Grey, White
 	
-	set_bkg_data(0,11,bgtiles);
+	set_bkg_data(0,10,bgtiles);
 	
 	//set_bkg_data(0, 4, tileData);
 	
@@ -28,7 +28,7 @@ void main()
 		*(UWORD*)(0x9800 + i) = 10;
 	}*/
 
-	set_bkg_tiles(0,0,18,20,background);
+	set_bkg_tiles(0,0,20,19,background);
 
 
 	//set_bkg_tiles(0, 0, 20, 18, tileMap);
