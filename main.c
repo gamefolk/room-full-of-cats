1eb402ff6da5e40843d963aea2be835ae488a562
Andy Russell <arussell123@gmail.com>
2013-04-14 16:44:13 -0700

    re-enabled interrupts

--- main.c
+++ main.c
@@ -12,14 +12,14 @@ void main() {
     init_music();
     while(1) {
         wait_vbl_done();
-//        disable_interrupts();
+        disable_interrupts();
 
         // Game loop begins here
         play_music(should_loop);
         do_gameplay();
         // Game loop ends here
 
-//        enable_interrupts();
+        enable_interrupts();
     }
 }
 
