dbfc502d9293403f9c2140d0902a353647e1137f
smpsnr <smpsnr@gmail.com>
2014-04-18 18:49:59 -0700

    add new music

--- music/songs_def.c
+++ music/songs_def.c
@@ -2,12 +2,15 @@
 
 int patern ;
 int song_nbr ;
-int nbr_patern = 3 ;
+int nbr_patern = 5 ;
 
 UWORD *data_song_ptr;
 
-void patern_definition() {
-    if(patern == 0){ data_song_ptr = data_song_0 ; }
-    if(patern == 1){ data_song_ptr = data_song_1 ; }
-    if(patern == 2){ data_song_ptr = data_song_2 ; }
+void patern_definition()
+{
+         if(patern == 0){ data_song_ptr = data_song_0 ; }
+         if(patern == 1){ data_song_ptr = data_song_1 ; }
+         if(patern == 2){ data_song_ptr = data_song_2 ; }
+         if(patern == 3){ data_song_ptr = data_song_3 ; }
+         if(patern == 4){ data_song_ptr = data_song_4 ; }
 }
