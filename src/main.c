be66bb8eb6b5a33ddf4a96c8078da60f6e4dd4df
smpsnr <smpsnr@gmail.com>
2014-07-08 23:53:29 -0400

    add title tile data/ map. show title screen before gameplay

--- src/main.c
+++ src/main.c
@@ -3,8 +3,15 @@
 #include "gameplay.h"
 #include "LP1.297a.h"
 
-void main() {    
+#include "tiles/title.c"
+
+static void show_title();
+
+void main() {
     int clock01 = 0;
+    
+    show_title();
+    waitpad(255);    
     
     init_music();
     init_gameplay();
@@ -32,4 +39,16 @@ void main() {
     }
 }
 
-
+static void show_title() {
+    disable_interrupts();
+	DISPLAY_OFF;
+	
+	LCDC_REG = 0x01;
+	BGP_REG = OBP0_REG = OBP1_REG = 0xE4U;
+	
+    set_bkg_data(0, 255, title_tiledata);
+	set_bkg_tiles(0, 0, 20, 18, title_tilemap);
+	
+	DISPLAY_ON;
+	enable_interrupts();
+}
